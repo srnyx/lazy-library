f0a63d5182406f91581c7ee51fe0a21265d1e671
srnyx <25808801+srnyx@users.noreply.github.com>
2024-08-25 15:49:03 -0400

    Update dependencies
    Gradle Galaxy: 1.2.3 -> 1.3.1
    JDA: 5.0.2 -> 5.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,24 +2,20 @@ import xyz.srnyx.gradlegalaxy.data.pom.DeveloperData
 import xyz.srnyx.gradlegalaxy.data.pom.LicenseData
 import xyz.srnyx.gradlegalaxy.enums.Repository
 import xyz.srnyx.gradlegalaxy.enums.repository
-import xyz.srnyx.gradlegalaxy.utility.addCompilerArgs
-import xyz.srnyx.gradlegalaxy.utility.setupJava
+import xyz.srnyx.gradlegalaxy.utility.setupJda
 import xyz.srnyx.gradlegalaxy.utility.setupPublishing
 
 
 plugins {
     application
-    id("xyz.srnyx.gradle-galaxy") version "1.2.3"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.1"
     id("com.gradleup.shadow") version "8.3.0"
 }
 
-setupJava("xyz.srnyx", "3.1.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_19)
-application.mainClass.set("xyz.srnyx.lazylibrary.LazyLibrary")
-addCompilerArgs("-parameters")
+setupJda("5.1.0", "xyz.srnyx", "3.1.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
 
-repository(Repository.MAVEN_CENTRAL, Repository.JITPACK)
+repository(Repository.JITPACK)
 dependencies {
-    compileOnly("net.dv8tion", "JDA", "5.0.2") // JDA
     implementation("xyz.srnyx", "java-utilities", "a073202b43") // General Java utility library
     implementation("io.github.freya022", "BotCommands", "2.10.3") // Command framework
     implementation("org.spongepowered", "configurate-yaml", "4.1.2") // Config manager
