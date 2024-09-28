75b86121af757d996c21fe6320220de65a7c7c68
srnyx <25808801+srnyx@users.noreply.github.com>
2024-09-28 18:55:59 -0400

    Update dependencies
    Shadow: 8.3.0 -> 8.3.2
    Java Utilities: a073202b43 -> 0ee049d52d
    PostgreSQL: 42.7.3 -> 42.7.4
    HikariCP: 5.1.0 -> 6.0.0
    Logback Classic: 1.5.6 -> 1.5.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,19 +9,19 @@ import xyz.srnyx.gradlegalaxy.utility.setupPublishing
 plugins {
     application
     id("xyz.srnyx.gradle-galaxy") version "1.3.1"
-    id("com.gradleup.shadow") version "8.3.0"
+    id("com.gradleup.shadow") version "8.3.2"
 }
 
 setupJda("5.1.0", "xyz.srnyx", "3.1.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
 
 repository(Repository.JITPACK)
 dependencies {
-    implementation("xyz.srnyx", "java-utilities", "a073202b43") // General Java utility library
+    implementation("xyz.srnyx", "java-utilities", "0ee049d52d") // General Java utility library
     implementation("io.github.freya022", "BotCommands", "2.10.3") // Command framework
     implementation("org.spongepowered", "configurate-yaml", "4.1.2") // Config manager
-    implementation("org.postgresql", "postgresql", "42.7.3") // Database
-    implementation("com.zaxxer", "HikariCP", "5.1.0") // Database
-    implementation("ch.qos.logback", "logback-classic", "1.5.6") // Logging
+    implementation("org.postgresql", "postgresql", "42.7.4") // Database
+    implementation("com.zaxxer", "HikariCP", "6.0.0") // Database
+    implementation("ch.qos.logback", "logback-classic", "1.5.8") // Logging
 }
 
 setupPublishing(
