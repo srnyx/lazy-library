0da6778e906666904a2acb892ed3ff80ae934981
srnyx <25808801+srnyx@users.noreply.github.com>
2024-10-03 13:16:03 -0400

    Update dependencies
    Gradle Galaxy: 1.3.1 -> 1.3.2
    Shadow: 8.3.2 -> 8.3.3
    Java Utilities: 0ee049d52d -> 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,15 @@ import xyz.srnyx.gradlegalaxy.utility.setupPublishing
 
 plugins {
     application
-    id("xyz.srnyx.gradle-galaxy") version "1.3.1"
-    id("com.gradleup.shadow") version "8.3.2"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.2"
+    id("com.gradleup.shadow") version "8.3.3"
 }
 
 setupJda("5.1.0", "xyz.srnyx", "3.1.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
 
 repository(Repository.JITPACK)
 dependencies {
-    implementation("xyz.srnyx", "java-utilities", "0ee049d52d") // General Java utility library
+    implementation("xyz.srnyx", "java-utilities", "2.0.0") // General Java utility library
     implementation("io.github.freya022", "BotCommands", "2.10.3") // Command framework
     implementation("org.spongepowered", "configurate-yaml", "4.1.2") // Config manager
     implementation("org.postgresql", "postgresql", "42.7.4") // Database
