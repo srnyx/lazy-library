5bd2c16374bd37f3853c3cc65cbd8c5e05d4c8ca
srnyx <25808801+srnyx@users.noreply.github.com>
2025-02-16 20:47:37 -0500

    Update JDA
    5.2.2 -> 5.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("com.gradleup.shadow") version "8.3.6"
 }
 
-setupJda("5.2.2", "xyz.srnyx", "3.2.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
+setupJda("5.3.0", "xyz.srnyx", "3.2.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
 
 repository(Repository.JITPACK)
 dependencies {
