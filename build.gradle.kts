e7af7ab598e875aaf7759c172d547ee22bbacb80
srnyx <25808801+srnyx@users.noreply.github.com>
2025-03-28 20:08:42 -0400

    Update dependencies
    JDA: 5.3.0 -> 5.3.1
    Java Utilities: 94d5d9c055 -> 150bcfdf44
    HikariCP: 6.2.1 -> 6.3.0
    Logback Classic: 1.5.16 -> 1.5.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,16 +12,16 @@ plugins {
     id("com.gradleup.shadow") version "8.3.6"
 }
 
-setupJda("5.3.0", "xyz.srnyx", "3.2.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
+setupJda("5.3.1", "xyz.srnyx", "3.2.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
 
 repository(Repository.JITPACK)
 dependencies {
-    implementation("xyz.srnyx", "java-utilities", "94d5d9c055") // General Java utility library
+    implementation("xyz.srnyx", "java-utilities", "150bcfdf44") // General Java utility library
     implementation("io.github.freya022", "BotCommands", "2.10.4") // Command framework
     implementation("org.spongepowered", "configurate-yaml", "4.2.0") // Config manager
     implementation("org.postgresql", "postgresql", "42.7.5") // Database
-    implementation("com.zaxxer", "HikariCP", "6.2.1") // Database
-    implementation("ch.qos.logback", "logback-classic", "1.5.16") // Logging
+    implementation("com.zaxxer", "HikariCP", "6.3.0") // Database
+    implementation("ch.qos.logback", "logback-classic", "1.5.18") // Logging
     implementation("dev.freya02", "jda-emojis", "3.0.0") // Emojis
 }
 
