6c0c4398b0bb0633df8d5cefd927bee4a5e2b8aa
srnyx <25808801+srnyx@users.noreply.github.com>
2025-04-14 15:01:12 -0400

    Add missing Javadocs

--- src/main/java/xyz/srnyx/lazylibrary/config/LazyChannel.java
+++ src/main/java/xyz/srnyx/lazylibrary/config/LazyChannel.java
@@ -13,31 +13,73 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 
+/**
+ * A class to hold a channel ID and a {@link Supplier} for the {@link Guild}
+ * <br>Contains useful utility methods
+ *
+ * @param   <T> the type of channel
+ */
 public class LazyChannel<T extends Channel> extends Stringable {
+    /**
+     * The {@link Supplier} for the {@link Guild} that owns the channel
+     */
     @NotNull public final Supplier<Guild> guildSupplier;
+    /**
+     * The ID of the channel
+     */
     public final long id;
 
+    /**
+     * Creates a new {@link LazyChannel}
+     *
+     * @param   guildSupplier   {@link #guildSupplier}
+     * @param   id              {@link #id}
+     */
     public LazyChannel(@NotNull Supplier<Guild> guildSupplier, long id) {
         this.guildSupplier = guildSupplier;
         this.id = id;
     }
 
+    /**
+     * Creates a new {@link LazyChannel}
+     *
+     * @param   guildSupplier   {@link #guildSupplier}
+     * @param   node            a {@link ConfigurationNode} containing the channel ID
+     */
     public LazyChannel(@NotNull Supplier<Guild> guildSupplier, @NotNull ConfigurationNode node) {
         this.guildSupplier = guildSupplier;
         this.id = node.getLong();
     }
 
+    /**
+     * Gets the mention of the channel
+     *
+     * @return  the mention of the channel
+     */
     @NotNull
     public String getMention() {
         return "<#" + id + ">";
     }
 
+    /**
+     * Gets the {@link Channel} from the {@link #guildSupplier}
+     *
+     * @return  the {@link Channel} from the {@link #guildSupplier}
+     */
     @NotNull
     public Optional<T> getChannel() {
         final Guild guild = guildSupplier.get();
         return guild == null ? Optional.empty() : getChannel(guild);
     }
 
+    /**
+     * Gets the {@link Channel} from the given {@link Guild}
+     * <br>This skips retrieving the {@link Guild} from {@link #guildSupplier} (for performance)
+     *
+     * @param   guild   the {@link Guild} to get the {@link Channel} from (should match {@link #guildSupplier})
+     *
+     * @return          the {@link Channel} from the given {@link Guild}
+     */
     @NotNull
     public Optional<T> getChannel(@NotNull Guild guild) {
         return Optional.ofNullable((T) guild.getGuildChannelById(id));
--- src/main/java/xyz/srnyx/lazylibrary/config/LazyRole.java
+++ src/main/java/xyz/srnyx/lazylibrary/config/LazyRole.java
@@ -20,55 +20,130 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 
+/**
+ * A class to hold a role ID and a {@link Supplier} for the {@link Guild}
+ * <br>Contains useful utility methods
+ */
 public class LazyRole extends Stringable {
+    /**
+     * The {@link LazyLibrary bot} instance
+     */
     @NotNull public final LazyLibrary bot;
+    /**
+     * The {@link Supplier} for the {@link Guild} that owns the role
+     */
     @NotNull public final Supplier<Guild> guildSupplier;
+    /**
+     * The ID of the role
+     */
     public final long id;
 
+    /**
+     * Creates a new {@link LazyRole}
+     *
+     * @param   bot             {@link #bot}
+     * @param   guildSupplier   {@link #guildSupplier}
+     * @param   id              {@link #id}
+     */
     public LazyRole(@NotNull LazyLibrary bot, @NotNull Supplier<Guild> guildSupplier, long id) {
         this.bot = bot;
         this.guildSupplier = guildSupplier;
         this.id = id;
     }
 
+    /**
+     * Creates a new {@link LazyRole}
+     *
+     * @param   bot             {@link #bot}
+     * @param   guildSupplier   {@link #guildSupplier}
+     * @param   node            a {@link ConfigurationNode} containing the role ID
+     */
     public LazyRole(@NotNull LazyLibrary bot, @NotNull Supplier<Guild> guildSupplier, @NotNull ConfigurationNode node) {
         this.bot = bot;
         this.guildSupplier = guildSupplier;
         this.id = node.getLong();
     }
 
+    /**
+     * Gets the mention of the role
+     *
+     * @return  the mention of the role
+     */
     @NotNull
     public String getMention() {
         return "<@&" + id + ">";
     }
 
+    /**
+     * Gets the {@link Role} from the {@link #guildSupplier}
+     *
+     * @return  the {@link Role} from the {@link #guildSupplier}
+     */
     @NotNull
     public Optional<Role> getRole() {
         final Guild guild = guildSupplier.get();
         return guild == null ? Optional.empty() : getRole(guild);
     }
 
+    /**
+     * Gets the {@link Role} from the given {@link Guild}
+     * <br>This skips retrieving the {@link Guild} from {@link #guildSupplier} (for performance)
+     *
+     * @param   guild   the {@link Guild} to get the {@link Role} from (should match {@link #guildSupplier})
+     *
+     * @return          the {@link Role} from the given {@link Guild}
+     */
     @NotNull
     public Optional<Role> getRole(@NotNull Guild guild) {
         return Optional.ofNullable(guild.getRoleById(id));
     }
 
+    /**
+     * Checks if the given {@link Member} has the role
+     *
+     * @param   member  the {@link Member} to check
+     *
+     * @return          {@code true} if the given {@link Member} has the role, {@code false} otherwise
+     */
     public boolean hasRole(@NotNull Member member) {
         for (final Role role : member.getRoles()) if (role.getIdLong() == id) return true;
         return false;
     }
 
+    /**
+     * Checks if the given user ID has the role
+     *
+     * @param   guild   the {@link Guild} to retrieve the {@link Member} and {@link Role} from (should match {@link #guildSupplier})
+     * @param   userId  the user ID to check
+     *
+     * @return          {@code true} if the given user ID has the role, {@code false} if the user isn't in the guild or doesn't have the role
+     */
     public boolean hasRole(@NotNull Guild guild, long userId) {
         return MiscUtility.handleException(() -> guild.retrieveMemberById(userId).complete())
                 .filter(this::hasRole)
                 .isPresent();
     }
 
+    /**
+     * Checks if the given user ID has the role
+     *
+     * @param   userId  the user ID to check
+     *
+     * @return          {@code true} if the given user ID has the role, {@code false} if the user isn't in the guild or doesn't have the role
+     */
     public boolean hasRole(long userId) {
         final Guild guild = guildSupplier.get();
         return guild != null && hasRole(guild, userId);
     }
 
+    /**
+     * Checks if the given {@link GuildSlashEvent} has the role and replies with an error message if not
+     * <br>It will check the {@link Member} from {@link GuildSlashEvent#getMember()}
+     *
+     * @param   event   the {@link GuildSlashEvent} to get the user from and reply to (if needed)
+     *
+     * @return          {@code true} if the user <b>doesn't</b> have the role, {@code false} if they <b>do</b>
+     */
     public boolean checkDontHaveRole(@NotNull GuildSlashEvent event) {
         final boolean hasRole = hasRole(event.getMember());
         if (!hasRole) event.replyEmbeds(LazyEmbed.noPermission(getMention()).build(bot)).setEphemeral(true).queue();
