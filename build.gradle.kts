969ba76d4595082fd8f4336eaa9e9cfb30b31d15
srnyx <25808801+srnyx@users.noreply.github.com>
2025-07-23 23:17:46 -0400

    Update dependencies
    Shadow: 8.3.7 -> 8.3.8
    JDA: 5.5.1 -> 5.6.1
    Java Utilities: f82100a462 -> 086e5ca712
    PostgreSQL: 42.7.6 -> 42.7.7
    Gradle: 8.14.2 -> 8.14.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,17 +9,17 @@ import xyz.srnyx.gradlegalaxy.utility.setupPublishing
 plugins {
     application
     id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.7"
+    id("com.gradleup.shadow") version "8.3.8"
 }
 
-setupJda("5.5.1", "xyz.srnyx", "3.2.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
+setupJda("5.6.1", "xyz.srnyx", "3.2.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
 
 repository(Repository.JITPACK)
 dependencies {
-    implementation("xyz.srnyx", "java-utilities", "f82100a462") // General Java utility library
+    implementation("xyz.srnyx", "java-utilities", "086e5ca712") // General Java utility library
     implementation("io.github.freya022", "BotCommands", "2.10.4") // Command framework
     implementation("org.spongepowered", "configurate-yaml", "4.2.0") // Config manager
-    implementation("org.postgresql", "postgresql", "42.7.6") // Database
+    implementation("org.postgresql", "postgresql", "42.7.7") // Database
     implementation("com.zaxxer", "HikariCP", "6.3.0") // Database
     implementation("ch.qos.logback", "logback-classic", "1.5.18") // Logging
     implementation("dev.freya02", "jda-emojis", "3.0.0") // Emojis
