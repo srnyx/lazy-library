c9b0aca16dfe69610ff8e0de4571550e124bcab3
srnyx <25808801+srnyx@users.noreply.github.com>
2025-08-01 04:07:21 -0400

    Update Java
    21 -> 22

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("com.gradleup.shadow") version "8.3.8"
 }
 
-setupJda("5.6.1", "xyz.srnyx", "3.2.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_21)
+setupJda("5.6.1", "xyz.srnyx", "3.2.0", "A simple library for JDA Discord bots", JavaVersion.VERSION_22)
 
 repository(Repository.JITPACK)
 dependencies {
