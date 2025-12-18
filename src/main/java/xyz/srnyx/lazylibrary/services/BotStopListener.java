eb6105347ac4e4f5de79b74013d2f3ee87859533
srnyx <25808801+srnyx@users.noreply.github.com>
2025-12-18 16:11:18 -0500

    Fix BotStopListener

--- src/main/java/xyz/srnyx/lazylibrary/services/BotStopListener.java
+++ src/main/java/xyz/srnyx/lazylibrary/services/BotStopListener.java
@@ -1,10 +1,9 @@
 package xyz.srnyx.lazylibrary.services;
 
-import io.github.freya022.botcommands.api.core.service.annotations.BService;
 import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService;
 
 
-@BService @InterfacedService(acceptMultiple = true)
+@InterfacedService(acceptMultiple = true)
 public interface BotStopListener {
     void onStop();
 }
