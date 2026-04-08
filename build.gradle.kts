fc2ad1dd44be10fc5ddb3240266e4fe60cfa6799
srnyx <25808801+srnyx@users.noreply.github.com>
2026-04-07 20:45:16 -0400

    Use new publishing system

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,19 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.kotlin.dsl.withType
 import xyz.srnyx.gradlegalaxy.data.config.DependencyConfig
 import xyz.srnyx.gradlegalaxy.data.config.JavaSetupConfig
+import xyz.srnyx.gradlegalaxy.data.config.publishingSimpleConfig
 import xyz.srnyx.gradlegalaxy.data.pom.DeveloperData
 import xyz.srnyx.gradlegalaxy.data.pom.LicenseData
 import xyz.srnyx.gradlegalaxy.enums.Repository
 import xyz.srnyx.gradlegalaxy.enums.repository
 import xyz.srnyx.gradlegalaxy.utility.setupJda
-import xyz.srnyx.gradlegalaxy.utility.setupPublishing
+import xyz.srnyx.gradlegalaxy.utility.setupPublishingEnv
 
 
 plugins {
     application
     `java-library`
-    id("xyz.srnyx.gradle-galaxy") version "2.0.2"
+    id("xyz.srnyx.gradle-galaxy") version "2.1.0"
     id("com.gradleup.shadow") version "8.3.9"
     id("dev.reformator.stacktracedecoroutinator") version "2.6.2"
 }
@@ -25,25 +26,26 @@ setupJda(
         description = "A simple library for JDA Discord bots"),
     jdaConfig = DependencyConfig(version = "6.4.1"))
 
-repository(Repository.JITPACK)
+repository(Repository.SRNYX_RELEASES, Repository.SRNYX_SNAPSHOTS)
 dependencies {
-    api("xyz.srnyx", "java-utilities", "b10e8b2497") // General Java utility library
-    api("com.google.code.gson", "gson", "2.3.1") // Use this specific version for Java Utilities
-    api("io.github.freya022", "BotCommands", "3.1.0") // Command framework
-    api("org.spongepowered", "configurate-yaml", "4.2.0") // Config manager
-    api("dev.freya02", "jda-emojis", "4.1.0") // Emojis
-    implementation("ch.qos.logback", "logback-classic", "1.5.32") // Logging
-    implementation("com.zaxxer", "HikariCP", "7.0.2") // Database
-    implementation("org.flywaydb", "flyway-core", "12.3.0") // Database
-    runtimeOnly("org.flywaydb", "flyway-database-postgresql", "12.3.0") // Database
-    runtimeOnly("org.postgresql", "postgresql", "42.7.10") // Database
+    api("xyz.srnyx:java-utilities:4a0f974") // General Java utility library
+    api("com.google.code.gson:gson:2.3.1") // Use this specific version for Java Utilities
+    api("io.github.freya022:BotCommands:3.1.0") // Command framework
+    api("org.spongepowered:configurate-yaml:4.2.0") // Config manager
+    api("dev.freya02:jda-emojis:4.1.0") // Emojis
+    implementation("ch.qos.logback:logback-classic:1.5.32") // Logging
+    implementation("com.zaxxer:HikariCP:7.0.2") // Database
+    implementation("org.flywaydb:flyway-core:12.3.0") // Database
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:12.3.0") // Database
+    runtimeOnly("org.postgresql:postgresql:42.7.10") // Database
 }
 
 // Fix Java's service loading, which Flyway uses
 tasks.withType<ShadowJar> { mergeServiceFiles() }
 
-setupPublishing(
-    artifactId = "lazy-library",
-    url = "https://lazy-library.srnyx.com",
-    licenses = listOf(LicenseData.MIT),
-    developers = listOf(DeveloperData.srnyx))
+setupPublishingEnv(
+    publishingSimpleConfig(
+        artifactId = "lazy-library",
+        url = "https://lazy-library.srnyx.com",
+        licenses = listOf(LicenseData.MIT),
+        developers = listOf(DeveloperData.srnyx)))
