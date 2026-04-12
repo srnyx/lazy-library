2ccbfa6108cb9ec09d04c1e56e6452615182eb82
srnyx <25808801+srnyx@users.noreply.github.com>
2026-04-12 19:09:37 -0400

    Update Java Utilities
    4a0f974 -> 092d56e

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ setupJda(
 
 repository(Repository.SRNYX_RELEASES, Repository.SRNYX_SNAPSHOTS)
 dependencies {
-    api("xyz.srnyx:java-utilities:4a0f974") // General Java utility library
+    api("xyz.srnyx:java-utilities:092d56e") // General Java utility library
     api("com.google.code.gson:gson:2.3.1") // Use this specific version for Java Utilities
     api("io.github.freya022:BotCommands:3.1.0") // Command framework
     api("org.spongepowered:configurate-yaml:4.2.0") // Config manager
