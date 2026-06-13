7caada2b1926ddf951f238b8821ebc63b30cea20
srnyx <25808801+srnyx@users.noreply.github.com>
2026-06-13 00:11:31 -0400

    Downgrade Flyway
    Back to original
    12.8.1 -> 12.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     api("dev.freya02:jda-emojis:4.3.0") // Emojis
     implementation("ch.qos.logback:logback-classic:1.5.34") // Logging
     implementation("com.zaxxer:HikariCP:7.0.2") // Database
-    implementation("org.flywaydb:flyway-core:12.8.1") // Database
+    implementation("org.flywaydb:flyway-core:12.3.0") // Database
     runtimeOnly("org.flywaydb:flyway-database-postgresql:12.8.1") // Database
     runtimeOnly("org.postgresql:postgresql:42.7.11") // Database
 }
